1e5a61030499b21014082b64b453b2578e5d0d80
Paul O'Connor <jamespauloconnor@gmail.com>
2015-08-26 21:37:00 -0400

    Initial Commit

---
+++ SwiftTrivia/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftTrivia
+//
+//  Created by Paul O'Connor on 8/26/15.
+//  Copyright (c) 2015 OCApps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
